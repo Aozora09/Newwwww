9d0eb2ac811f85e9b9b42a958047cf4164192b5c
Aozora09 <azharirambe2003@gmail.com>
2023-05-24 13:28:01 +0800

    Pengumpulan Tugas PMB

---
+++ script.js
@@ -0,0 +1,11 @@
+//Klik Gambar ke Link
+function submitform(){
+  alert("Pesan Sudah Dikirim");
+}
+function OpenmyIG() {
+  // Logika atau aksi tambahan saat gambar diklik
+  window.open('https://www.instagram.com/azhari_aozora/?igshid=ZDc4ODBmNjlmNQ%3D%3D');
+}
+function openlink(){
+  window.open('https://www.youtube.com/@HajiKoding')
+}
